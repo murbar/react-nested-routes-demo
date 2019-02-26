dbef4880cc9e83aff936518ef9ca22599fad0e8f
Joel Bartlett <joel@murbar.com>
2019-02-26 17:44:35 -0500

    Add initial routes

--- nested-routes/src/App.js
+++ nested-routes/src/App.js
@@ -63,22 +63,13 @@ const catalog = [
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <MainNav />
+          <Route exact path="/" component={Home} />
+          <Route path="/books" component={Books} />
       </div>
+      </Router>
     );
   }
 }
