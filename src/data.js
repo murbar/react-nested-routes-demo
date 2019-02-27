274b045f8bd88e948efa7097e0a0955769bf92b2
Joel Bartlett <joel@murbar.com>
2019-02-26 20:48:18 -0500

    Extract app data

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const catalog = [
+  {
+    id: 1,
+    slug: 'fiction',
+    title: 'Fiction',
+    books: [
+      {
+        id: 1,
+        slug: 'great-gatsby',
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        description:
+          'The Great Gatsby, F. Scott Fitzgerald’s third book, stands as the supreme achievement of his career. First published in 1925, this quintessential novel of the Jazz Age has been acclaimed by generations of readers. The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted “gin was the national drink and sex the national obsession,” it is an exquisitely crafted tale of America in the 1920s.'
+      },
+      {
+        id: 2,
+        slug: 'to-kill-a-mockingbird',
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        description:
+          'One of the most cherished stories of all time, To Kill a Mockingbird has been translated into more than forty languages, sold more than forty million copies worldwide, served as the basis for an enormously popular motion picture, and was voted one of the best novels of the twentieth century by librarians across the country. A gripping, heart-wrenching, and wholly remarkable tale of coming-of-age in a South poisoned by virulent prejudice, it views a world of great beauty and savage inequities through the eyes of a young girl, as her father—a crusading local lawyer—risks everything to defend a black man unjustly accused of a terrible crime.'
+      },
+      {
+        id: 3,
+        slug: '1984',
+        title: '1984',
+        author: 'George Orwell',
+        description:
+          'A startling and haunting vision of the world, 1984 is so powerful that it is completely convincing from start to finish. No one can deny the influence of this novel, its hold on the imaginations of multiple generations of readers, or the resiliency of its admonitions—a legacy that seems only to grow with the passage of time.'
+      }
+    ]
+  },
+  {
+    id: 2,
+    slug: 'non-fiction',
+    title: 'Non-Fiction',
+    books: [
+      {
+        id: 1,
+        slug: 'art-of-war',
+        title: 'The Art of War',
+        author: 'Sun Tzu',
+        description:
+          'The Art of War is an ancient Chinese military treatise attributed to Sun Tzu a high-ranking military general, strategist and tactician, and it was believed to have been compiled during the late Spring and Autumn period or early Warring States period. The text is composed of 13 chapters, each of which is devoted to one aspect of warfare. It is commonly known to be the definitive work on military strategy and tactics of its time. It has been the most famous and influential of China’s Seven Military Classics, and for the last two thousand years it remained the most important military treatise in Asia, where even the common people knew it by name. It has had an influence on Eastern and Western military thinking, business tactics, legal strategy and beyond.'
+      },
+      {
+        id: 2,
+        slug: 'on-liberty',
+        title: 'On Liberty',
+        author: 'John Stuart Mill',
+        description:
+          'The British economist, philosopher, and ethical theorist\'s argument does not focus on "the so-called Liberty of the Will…but Civil, or Social Liberty: the nature and limits of the power which can be legitimately exercised by society over the individual." Mill asks and answers provocative questions relating to the boundaries of social authority and individual sovereignty.In powerful and persuasive prose, he declares that there is "one very simple principle" regarding the use of coercion in society — one may only coerce others either to defend oneself or to defend others from harm.'
+      },
+      {
+        id: 3,
+        slug: 'republic',
+        title: 'The Republic',
+        author: 'Plato',
+        description:
+          'In search of an ideal civilization, Socrates leads Glaucon, Polemarchus, Thrasymachus, and others in debates about various subjects, including justice, truth, class, and art. For without righteousness, tyranny and injustice give rise to oligarchy. The influential dialogues of The Republic helped shape all of Western literature and philosophical thought.It is as much a doctrine of ethics and politics now as it was for the ancient Greeks, and its dilemma remains: how to create a perfect society populated by very imperfect human beings.'
+      }
+    ]
+  }
+];
+
+export default catalog;
