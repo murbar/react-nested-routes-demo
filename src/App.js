ffe6ecbfc947b9bddaa5ef00285cc6a61bcec7da
Joel Bartlett <joel@murbar.com>
2019-02-26 20:48:48 -0500

    Migrate app data

--- src/App.js
+++ src/App.js
@@ -1,65 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
-
-const catalog = [
-  {
-    id: 1,
-    slug: 'fiction',
-    title: 'Fiction',
-    books: [
-      {
-        id: 1,
-        slug: 'great-gatsby',
-        title: 'The Great Gatsby',
-        description:
-          'Quos, illo expedita! Quo, unde? Mollitia nobis libero voluptatibus dolorum quaerat! Inventore praesentium libero quos voluptatum fuga ab, cumque est voluptatibus ullam animi quidem placeat porro, sit dolores neque iusto.'
-      },
-      {
-        id: 2,
-        slug: 'to-kill-a-mockingbird',
-        title: 'To Kill a Mockingbird',
-        description:
-          'Distinctio architecto culpa natus soluta beatae alias debitis omnis earum dicta consequuntur odio quos incidunt maiores pariatur fuga quibusdam, optio neque quaerat libero placeat molestiae accusamus nesciunt itaque.'
-      },
-      {
-        id: 3,
-        slug: '1984',
-        title: '1984',
-        description:
-          'Ipsam ea rerum nihil ratione vel officia quos. Culpa vero quae voluptates quia aliquid architecto! Atque, adipisci. Totam nostrum fugit ex vero commodi optio quae. Aut at sint ullam labore.'
-      }
-    ]
-  },
-  {
-    id: 2,
-    slug: 'non-fiction',
-    title: 'Non-Fiction',
-    books: [
-      {
-        id: 1,
-        slug: 'art-of-war',
-        title: 'The Art of War',
-        description:
-          'Natus quod repellat iste assumenda voluptatibus animi laboriosam distinctio minima illo voluptatum? Eum nesciunt aspernatur quas quos. Ab eligendi libero minus, voluptas consequatur explicabo dolor molestiae. Nulla incidunt aliquam impedit!'
-      },
-      {
-        id: 2,
-        slug: 'on-liberty',
-        title: 'On Liberty',
-        description:
-          'Voluptatum id ad necessitatibus? Recusandae cumque soluta odit placeat dicta, molestias numquam, corrupti sint asperiores voluptatibus dolorem minima cum, ex iure consectetur maxime. Quisquam libero veritatis, minima illo minus dolorem.'
-      },
-      {
-        id: 3,
-        slug: 'republic',
-        title: 'The Republic',
-        description:
-          'Vel totam, eligendi dicta, ut nostrum consequuntur odio, voluptate laudantium cupiditate quis illum tempora provident excepturi! Assumenda temporibus, quae accusamus libero maxime laboriosam similique adipisci nostrum. Aliquam fuga iste iusto.'
-      }
-    ]
-  }
-];
+import appData from './data';
 
 const MainNav = () => {
   return (
